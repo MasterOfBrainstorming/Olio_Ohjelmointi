9623affc5c3f289e8ffd01b417278579bf61a719
Mikael Romanov <mikael.romanov@hotmail.com>
2018-03-08 18:09:47 +0200

    Add files via upload

---
+++ Demo3/Tuntiharjoitus/Class3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuntiharjoitus_2
+{
+    class Student: Person
+    {
+        public string StudentID { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(string firstname, string lastname, string studenID)
+            :base(firstname,lastname)
+        {
+            StudentID = StudentID;
+        }
+
+        public void StudentMethod()
+        {
+            Console.WriteLine("This method belongs to Student");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " " + StudentID;
+        }
+    }
+}
---
+++ Demo3/Tuntiharjoitus/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuntiharjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person = new Person();
+            //person.FirstName = "Pekka";
+
+            // create one student object
+            Student student = new Student();
+            student.FirstName = "Nakki";
+            student.LastName = "Naksu";
+            student.Address = "Nakkikuja";
+            student.Age = 23;
+            student.PhoneNumber = "10000000";
+            student.StudentID = "G4920";
+            Console.WriteLine(student.ToString());
+
+            Teacher teacher = new Teacher("Sammpo","Verkkomiäs","D420");
+            teacher.Age = 57;
+            teacher.Address = "himassa";
+            teacher.PhoneNumber = "010210240";
+            Console.WriteLine(teacher.ToString());
+
+
+            teacher.TeacherMethod();
+            student.StudentMethod();
+
+            // olio ei ole aliluokan sisällä...
+            // student.PersonMethod();
+        }
+
+    }
+}
