8c31284ca9663789ff5f834ee456ca965a991a4b
Mikael Romanov <mikael.romanov@hotmail.com>
2018-03-08 18:04:59 +0200

    Add files via upload

---
+++ Demo5/Tuntiharjoitus/Persons.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekisteri
+{
+    class Persons
+    {
+        private List<Person> persons;
+
+
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+
+        /// <summary>
+        /// Add person to collection
+        /// </summary>
+        /// <param name="person">person to add</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        public Person GetPerson(int location)
+        {
+            if (location < persons.Count)
+            {
+               return persons.ElementAt(location);
+            }
+            else { return null; }
+        }
+
+        public Person FindPerson(string social)
+        {
+            foreach (Person person in persons)
+            {
+                if (social.Equals(person.SocialSecurityNumber))
+                {
+                    return person;
+                }
+            }
+            return null;
+        }
+
+
+        public void Print()
+        {
+            foreach(Person person in persons)
+            {
+                Console.Write(person);
+            }
+        }
+    }
+}
---
+++ Demo5/Tuntiharjoitus/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekisteri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persons jamit = new Persons();
+
+            Person jami1 = new Person { Firstname="Dj",Lastname="Kridlokk",SocialSecurityNumber="KCMD-MAFIA" };
+            Person jami2 = new Person { Firstname = "Evil", Lastname = "Stöö", SocialSecurityNumber = "ROUTSI-STÖGIS" };
+            Person jami3 = new Person { Firstname = "Koksu", Lastname = "Koo", SocialSecurityNumber = "LOMMO" };
+
+            // ADD Person objects to Persons (jamit)
+            jamit.AddPerson(jami1);
+            jamit.AddPerson(jami2);
+            jamit.AddPerson(jami3);
+
+            // get one person
+
+            Person jami4 = jamit.GetPerson(0);
+            if (jami4 != null)
+            {
+                Console.WriteLine(jami4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Nakki");
+            }
+
+            jamit.Print();
+
+            string sotu = "KCMD-MAFIA";
+            Console.Write("Find with: {0} \n", sotu);
+            Person jami5 = jamit.FindPerson(sotu);
+            if (jami5 != null)
+            {
+                Console.WriteLine(jami5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("_Super_Nakki_Tällä_Pistetään_Paikat_Remonttiin");
+            }
+        }
+    }
+}
